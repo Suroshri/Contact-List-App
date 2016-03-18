6be6f5884e192c1d13446948c96b8a41d5db26e1
Suroshri <suroshri@gmail.com>
2016-03-18 19:56:09 +0530

    first commit

---
+++ public/controllers/controller.js
@@ -0,0 +1,54 @@
+/**
+ * Created by suroshri on 18/3/16.
+ */
+
+angular.module('contactListApp',[])
+    .controller('ContactController', ['$scope', '$http', Contactcontroller]);
+
+function Contactcontroller($scope, $http){
+    console.log("Hello from controller");
+
+    var refresh = function() {
+        $http.get('/contactlist').success(function (response) {
+            console.log('I received the data I requested');
+            $scope.contactlist = response;
+            $scope.contact = "";
+        });
+    }
+
+    refresh();
+
+    $scope.addContact = function() {
+        console.log($scope.contact);
+        $http.post('/contactlist', $scope.contact).success(function(response){
+            console.log(response);
+            refresh();
+        });
+    };
+
+    $scope.remove = function(id) {
+        console.log(id);
+        $http.delete('/contactlist/'+id).success(function(response){
+            console.log(response);
+            refresh();
+        });
+    }
+
+    $scope.edit = function (id) {
+        console.log(id);
+        $http.get('/contactlist/'+id).success(function(response){
+            $scope.contact = response;
+        });
+    }
+
+    $scope.update = function () {
+        $http.put('/contactlist/'+$scope.contact._id, $scope.contact).success(function(response){
+            console.log(response);
+            refresh();
+        });
+    }
+
+    $scope.clear = function() {
+        $scope.contact = "";
+    }
+}
